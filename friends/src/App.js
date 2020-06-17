4247e312f48b169efc9e00bdcb6fdab92d525b16
hera <a.heraimenka@gmail.com>
2020-06-16 19:05:18 -0700

    Show FriendList component

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,21 +4,28 @@ import { Container } from 'reactstrap';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import './App.scss';
+import PrivateRoute from './utils/privateRoute';
+import FriendList from './components/FriendList/FriendList';
 
 function App() {
     const token = localStorage.getItem('loginToken');
-    const [loggedIn, setLoggedIn] = useState(token && false);
+    const [loggedIn, setLoggedIn] = useState(Boolean(token));
 
     return (
         <Container>
             <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
             <Switch>
+
                 <Route exact path="/">
-                    <Login setLoggedIn={setLoggedIn} />
+                    <Redirect to="/friends" />
                 </Route>
+
                 <Route path="/login">
                     <Login setLoggedIn={setLoggedIn} />
                 </Route>
+
+                <PrivateRoute path="/friends" component={FriendList} />
+
             </Switch>
         </Container>
     );
---
+++ friends/src/components/FriendList/FriendList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function FriendList (props) {
+    return (
+        <div>friends here</div>
+    );
+}
